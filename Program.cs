bd22cc42629208443ee5d53f694ba27183b8ae24
CSUK\csukppcy <philip.cottray@creditsafe.com>
2021-05-12 09:29:33 +0100

    add build tasks

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mslearn_tailspin_spacegame_web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
